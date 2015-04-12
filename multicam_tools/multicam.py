60a07f3ba1df154bc3994ddc54e563491ab5dfcd
nocarryr <matt@nomadic-recording.com>
2015-04-12 12:36:03 -0500

    add subclasses to simplify code

--- multicam_tools/multicam.py
+++ multicam_tools/multicam.py
@@ -1,10 +1,38 @@
 import bpy
 
-class MultiCam(object):
-    def __init__(self, mc):
-        self.mc = mc
-        self.cuts = {}
-        self.strips = {}
+class BlendObj(object):
+    def __init__(self, **kwargs):
+        self.context = kwargs.get('context')
+        self.blend_obj = kwargs.get('blend_obj')
+        if not hasattr(self, 'fcurve_property'):
+            self.fcurve_property = kwargs.get('fcurve_property')
+    @property
+    def blend_obj(self):
+        return getattr(self, '_blend_obj', None)
+    @blend_obj.setter
+    def blend_obj(self, value):
+        old = self.blend_obj
+        if value == old:
+            return
+        self._blend_obj = value
+        self.on_blend_obj_set(value, old)
+    def on_blend_obj_set(self, new, old):
+        self._fcurve = None
+    @property
+    def context(self):
+        context = getattr(self, '_context', None)
+        if context is None:
+            context = bpy.context
+        return context
+    @context.setter
+    def context(self, value):
+        old = getattr(self, '_context', None)
+        if old == value:
+            return
+        self._context = value
+        self.on_context_set(value, old)
+    def on_context_set(self, new, old):
+        self._fcurve = None
     @property
     def fcurve(self):
         fc = getattr(self, '_fcurve', None)
@@ -12,14 +40,23 @@ class MultiCam(object):
             fc = self._fcurve = self.get_fcurve()
         return fc
     def get_fcurve(self):
-        mc_path = self.mc.path_from_id()
-        action = bpy.context.scene.animation_data.action
+        path = self.blend_obj.path_from_id()
+        action = self.context.scene.animation_data.action
+        prop = self.fcurve_property
         for fc in action.fcurves.values():
-            if mc_path not in fc.data_path:
+            if path not in fc.data_path:
                 continue
-            if 'multicam_source' not in fc.data_path:
+            if fc.data_path.split('.')[-1] != prop:
                 continue
             return fc
+    
+    
+class MultiCam(BlendObj):
+    fcurve_property = 'multicam_source'
+    def __init__(self, **kwargs):
+        super(MultiCam, self).__init__(**kwargs)
+        self.cuts = {}
+        self.strips = {}
     def iter_keyframes(self):
         for kf in self.fcurve.keyframe_points.values():
             yield kf.co
@@ -32,32 +69,18 @@ class MultiCam(object):
         for strip in self.strips.values():
             strip.build_keyframes()
     def get_strip_from_channel(self, channel):
-        for s in bpy.context.scene.sequence_editor.sequences:
+        for s in self.context.scene.sequence_editor.sequences:
             if s.channel == channel:
-                source = MulticamSource(self, s)
+                source = MulticamSource(context=self.context, blend_obj=s, multicam=self)
                 self.strips[channel] = source
                 return source
                 
-class MulticamSource(object):
-    def __init__(self, multicam, strip):
-        self.multicam = multicam
-        self.strip = strip
+class MulticamSource(BlendObj):
+    fcurve_property = 'blend_alpha'
+    def __init__(self, **kwargs):
+        super(MulticamSource, self).__init__(**kwargs)
+        self.multicam = kwargs.get('multicam')
         self._keyframe_data = None
-    @property
-    def fcurve(self):
-        fc = getattr(self, '_fcurve', None)
-        if fc is None:
-            fc = self._fcurve = self.get_fcurve()
-        return fc
-    def get_fcurve(self):
-        strip_path = self.strip.path_from_id()
-        action = bpy.context.scene.animation_data.action
-        for fc in action.fcurves.values():
-            if strip_path not in fc.data_path:
-                continue
-            if 'blend_alpha' not in fc.data_path:
-                continue
-            return fc
     @property
     def keyframe_data(self):
         d = self._keyframe_data
@@ -67,7 +90,7 @@ class MulticamSource(object):
     def build_keyframe_data(self):
         d = {}
         cuts = self.multicam.cuts
-        channel = self.strip.channel
+        channel = self.blend_obj.channel
         is_active = False
         for frame in sorted(cuts.keys()):
             cut = cuts[frame]
@@ -80,7 +103,7 @@ class MulticamSource(object):
         return d
     def insert_keyframe(self, frame, value, **kwargs):
         if self.fcurve is None:
-            self.strip.keyframe_insert('blend_alpha', frame=frame)
+            self.blend_obj.keyframe_insert('blend_alpha', frame=frame)
             kf = self.get_keyframe(frame)
             kf.co[1] = value
         else:
@@ -101,7 +124,7 @@ class MulticamSource(object):
             self.insert_keyframe(frame, value, interpolation='CONSTANT')
     
 def test():
-    mc = MultiCam(bpy.context.selected_sequences[0])
+    mc = MultiCam(blend_obj=bpy.context.selected_sequences[0])
     mc.build_cuts()
     mc.build_strip_keyframes()
 test()
